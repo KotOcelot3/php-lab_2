aee4727922016f9590103554f3702b8bf85f9072
KotOcelot3 <114900378+KotOcelot3@users.noreply.github.com>
2023-06-02 15:01:42 +0700

    Update php.php

--- localhost/php.php
+++ localhost/php.php
@@ -41,15 +41,7 @@ var_dump($massiveZ);
 
 if (in_array(7, $massiveZ) || in_array(17, $massiveZ) ||
     in_array(27, $massiveZ) || in_array(37, $massiveZ) ||
-    in_array(47, $massiveZ) || in_array(57, $massiveZ) ||
-    in_array(67, $massiveZ) || in_array(70, $massiveZ) ||
-    in_array(71, $massiveZ) || in_array(72, $massiveZ) ||
-    in_array(73, $massiveZ) || in_array(74, $massiveZ) ||
-    in_array(75, $massiveZ) || in_array(76, $massiveZ) ||
-    in_array(77, $massiveZ) || in_array(78, $massiveZ) ||
-    in_array(79, $massiveZ) || in_array(87, $massiveZ) ||
-    in_array(97, $massiveZ)) {
-
+    
     echo "МассивZ соддержит числа, в которых есть цифра 7";
 }  else {
     echo "МассивZ не соддержит числа, в котором есть цифра 7";
